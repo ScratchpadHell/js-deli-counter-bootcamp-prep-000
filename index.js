2561661767d2b5fdc7007856ce7cd1132a36329b
Jacques Bellisario <gypsyoak@yahoo.com>
2018-03-28 04:18:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ function nowServing(line) {
  }
  
  function currentLine(line) {
-   if (line.length === 0) {
-     return "The line is currently empty."
+   const namesAndNumbers = [];
+   for (i = 0, l = line.length; i < l; i++) {
+     
+     return `The line is currently:
    }
- return `The line is currently: 
+ 
+ return "The line is currently empty."
