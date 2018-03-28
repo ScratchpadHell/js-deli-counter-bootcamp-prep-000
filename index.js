1217483b8f435156c248dc7b39b9effbd62dec5b
Jacques Bellisario <gypsyoak@yahoo.com>
2018-03-28 04:03:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var katzDeliLine = [];
+
+function takeANumber(line, name) {
+  line.push(name)
+  
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
+}
+
+function nowServing(line) {
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+ return `Currently serving ${line.shift()}.`
+ }
+ 
+ function currentLine(line) {
+   if (line.length === 0) {
+     return "The line is currently empty."
+   }
+ return `The line is currently: 
